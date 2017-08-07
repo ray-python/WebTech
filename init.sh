4ecb57cdbf70a2372634bf11b1ea3ea9b1214a6e
ray-python <raypaython@yandex.ru>
2017-08-08 00:28:19 +0300

    Initial commit

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
 sudo /etc/init.d/gunicorn restart﻿﻿
 
-gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
-gunicorn -c /home/box/web/etc/django.py wsgi --daemon
+sudo gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
+sudo gunicorn -c /home/box/web/etc/django.py ask.wsgi:application --daemon
