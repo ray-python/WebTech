7ae2f918b9d44b110ba89a90323b97a776f00a07
ray-python <raypaython@yandex.ru>
2017-08-07 22:42:07 +0300

    Initial commit

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,4 +18,11 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', 'qa.views.test'),
+    url(r'^login/$', 'qa.views.test'),
+    url(r'^signup/$', 'qa.views.test'),
+    url(r'^question/(\d+)$', 'qa.views.test'),
+    url(r'^ask/.*$', 'qa.views.test'),
+    url(r'^popular/$', 'qa.views.test'),
+    url(r'^new/$', 'qa.views.test'),
 ]
