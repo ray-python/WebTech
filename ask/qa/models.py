d4a0858d0c721806a3ee19988df3112544e1c5b3
ray-python <raypaython@yandex.ru>
2017-08-14 22:22:29 +0300

    Initial commit

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class QuestionManager(models.Manager):
+    def resent_questions(self):
+        return self.order_by('-added_at')
+
+    def popular_questions(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=1)
+    author = models.ForeignKey(User, related_name='question_user', blank=True, null=True)
+    rating = models.BigIntegerField(default=1)
+    likes = models.ManyToManyField(User, related_name='%(app_label)s_%(class)s_related', blank=True)
+
+    objects = QuestionManager()
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('qa.views.question_details', [str(self.id)])
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question =  models.ForeignKey(Question)
+    author = models.ForeignKey(User, blank=True, null=True)
