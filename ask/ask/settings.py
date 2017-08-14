5458a1963bca7cc933bec51b5a36d44e63529e14
ray-python <raypaython@yandex.ru>
2017-08-14 22:35:28 +0300

    Initial commit

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(os.path.abspath(os.path.dirname(__file__)), 'qa.db'),
     }
 }
 
