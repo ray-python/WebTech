5a74d3f9700a450c03a6802fa48d9c3f655e5dfe
ray-python <raypaython@yandex.ru>
2017-09-20 22:37:26 +0300

    Initial commit

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from .models import Question
