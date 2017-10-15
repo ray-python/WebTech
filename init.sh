a9e08ca55410bf18be172945369f0da9ad8e3c7c
ray-python <raypaython@yandex.ru>
2017-10-15 14:49:59 +0300

    Initial commit

--- init.sh
+++ init.sh
@@ -3,3 +3,12 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
 sudo /etc/init.d/gunicorn restart﻿﻿
+
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE USER 'admin'@'localhost'"
+mysql -uroot -e "SET PASSWORD FOR 'admin'@'localhost' = PASSWORD('admin')"
+mysql -uroot -e "CREATE DATABASE qa"
+mysql -uroot -e "GRANT ALL ON qa.* TO 'admin'@'localhost'"
+cd /home/box/web/ask
+sudo python manage.py makemigrations qa
+sudo python manage.py migrate
