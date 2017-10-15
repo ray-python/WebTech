ec2f7aab64593ab95e4e361fc913606fcde576ee
ray-python <raypaython@yandex.ru>
2017-10-15 15:55:20 +0300

    Initial commit

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class QuestionManager(models.Manager):
     def new(self):
-        return self.order_by('-id')
+        return self.order_by('-added_at')
 
     def popular(self):
         return self.order_by('-rating')
@@ -15,12 +15,14 @@ class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
+    rating = models.IntegerField(default=1)
     author = models.ForeignKey(User, related_name='question_user', blank=True, null=True)
     likes = models.ManyToManyField(User, related_name='%(app_label)s_%(class)s_related', blank=True)
 
     objects = QuestionManager()
 
+    def get_absolute_url(self):
+        return ('qa.views.question_details', [str(self.id)])
 
 class Answer(models.Model):
     text = models.TextField()
